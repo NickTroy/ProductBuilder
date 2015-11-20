e3e1430dcb28ca19bbddd774a8c7d825dc91aa28
Mykyta <nicktroyanov@gmail.com>
2015-11-20 16:13:06 +0200

    add new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,23 @@ class ProductsController < AuthenticatedController
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
   end
 
+  def new
+    @product = ShopifyAPI::Product.new
+  end
+
+  def create
+    @product = ShopifyAPI::Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    end
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:body_html, :handle, :images, :options, 
+                                      :product_type, :published_scope, :tags,
+                                      :template_suffix, :title, :variants, :vendor)
+    end
+
 end
