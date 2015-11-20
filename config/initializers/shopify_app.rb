b96fe4a9d4adfaacfc2e632e6eda3432e00be26b
Mykyta <nicktroyanov@gmail.com>
2015-11-20 13:57:07 +0200

    redirect uri added to omniauth

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "e183d50516e620411040157674e8b304"
   config.secret = "e9d6d614db13892494ae4b22c849fa36"
-  config.redirect_uri = "http://productbuilder.herokuapp.com/auth/shopify/callback"
+  config.redirect_uri = "https://productbuilder.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
-  config.embedded_app = true
+  config.embedded_app = false
 end
