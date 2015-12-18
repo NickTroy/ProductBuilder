b033287d069bd3f971b745093ffa7b00c7ccda0c
nicktroy <nicktroyanov@gmail.com>
2015-12-18 17:56:48 +0000

    update options

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -41,7 +41,7 @@ class OptionsController < AuthenticatedController
     @option.name = params[:option][:name]
     @option.save
     1.upto(params[:number_of_option_values].to_i) do |i|
-      @option_value = OptionValue.new(option_id:@option.id, value:params["option_value#{i}".to_sym])
+      @option_value = OptionValue.where(value: params["option_value#{i}".to_sym])[0] || OptionValue.new(option_id:@option.id, value: params["option_value#{i}".to_sym])
       @option_value.save
     end
     if @option.save
