3f511ddb6691e89c30a292e777e8560f6af71e44
nicktroy <nicktroyanov@gmail.com>
2016-04-29 13:22:05 +0000

    search variants by sku

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -5,7 +5,6 @@ module Filterable
     def filter(filtering_params)
       results = self.where(nil)
       filtering_params.each do |key, value|
-        binding.pry
         results = results.public_send(key, value) if value.present?
       end
       results
