fe7e5e43eba9f5bc7f47258ea97ca2eecec42a10
Oleksy Ostanin <tnd_prof@yahoo.com>
2016-08-17 14:20:31 +0000

    Fix import

--- app/controllers/product_info_controller.rb
+++ app/controllers/product_info_controller.rb
@@ -95,20 +95,16 @@ class ProductInfoController < ApplicationController
     @first_image = ProductImage.where(product_id: params[:id]).first
     @option_dependency = []
     @options_count = @product_options.count
-    
-    query_for_variant_branches = "select  " 
-    @product_options.each_with_index do |option, index|
-      option_query_row = "(select ov#{index}.value as v#{index} from option_values ov#{index}, variants_option_values vov#{index} where vov#{index}.variant_id = v.id and vov#{index}.option_value_id = ov#{index}.id and ov#{index}.option_id = #{option[:option_id]}), "
-      query_for_variant_branches += option_query_row
+    @variants_option_values = @variants.map do |variant|
+      crap = Array.new
+      @product_options.map { |product_option| crap << OptionValue.where(:option_id => product_option[:option_id]).includes( :variants ).where( :variants => { id: variant.id} ).first.value }
+      crap << variant.id
     end
-    query_for_variant_branches.chop!.chop!
-    query_for_variant_branches_with_variant_ids = query_for_variant_branches + ", v.id from variants v where v.product_id = #{params[:id]}"
-    query_for_variant_branches += "from variants v where v.product_id = #{params[:id]}"
-    @variants_option_values = ActiveRecord::Base.connection.execute(query_for_variant_branches_with_variant_ids)
     @color_and_material_values = @variants_option_values.to_a.map { |branch| [branch[-3], branch[-2]] }.uniq
+
     build_option_dependency_tree
     respond_to do |format|
-      format.json #{ render status: :ok, :callback => params[:callback] }
+      format.json 
     end
   end  
   
@@ -125,7 +121,7 @@ class ProductInfoController < ApplicationController
   private
 
   def build_option_dependency_tree
-    @variants_option_values.each do |variant_option_values|#@variants.each do |variant|
+    @variants_option_values.each do |variant_option_values|
       @variant_branch = []
       variant_option_values.reverse.each do |option_value|
         @variant_branch = [option_value, @variant_branch]
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,7 +2,6 @@ module ProductsHelper
   DATA_ROW = 6
   TITLE_ROW = 0
   DESCR_ROW = 1
-  OPTIONS_COL_FROM = 20
   FORBIDDEN_FIELDS = ["created_at", "updated_at", "variants", "images", "image",  "handle", "published_at", 
                           "template_suffix", "published_scope", "tags", "options","id", "handle", "main_product_id", 
                           "created_at", "updated_at", "product_id", "product_image_id", "pseudo_product_id", 
@@ -103,20 +102,19 @@ module ProductsHelper
   
   def persist_product data, info, variants
     _product = ShopifyAPI::SmartCollection.where(title:"Product_builder_products").first.products.find { |product| product.title == data['title'] }
-    
     if _product.nil?
       _product = ShopifyAPI::Product.new data
-      _product.attributes[:tags] = "product" unless _product.attributes[:tags] == "product"
+      _product.attributes[:tags] = "product"
+      byebug
+      _product.attributes[:images] = data['images'].split(',') if data['images'].present?
+      
       _product.save
     end 
-    
      _product_info = ProductInfo.find_by(main_product_id: _product.id) || ProductInfo.create(:main_product_id => _product.id, :handle => _product.handle)
-     
     unless info['shipping_method_name'].nil?
       shipping_method = ShippingMethod.find_by(name: info['shipping_method_name'] ) || ShippingMethod.create(name: info['shipping_method_name'] )
       shipping_method.product_infos << _product_info
     end
-
     _product_info.update_attributes({
       "be_sure_to_note" => info["be_sure_to_note"],
       "why_we_love_this" => info["why_we_love_this"],
@@ -131,28 +129,9 @@ module ProductsHelper
     })
     
     0.upto( variants.length - 1 ) do |i|
-
-      variant = Variant.find_by(:product_id => _product.id, :sku => variants[i]['sku'])
-      if variant.blank? or variants[i]['sku'].blank?
-        variant = Variant.create(:product_id => _product.id) 
-      else
-        pseudo_product_title = "#{_product.title} "
-        o = variant.option_values.joins("join options on options.id = option_values.option_id").select("options.name as option_name, option_values.value as value")
-        color_option_value = o.find { |option| option.option_name == "Color" }
-        pseudo_product_title += color_option_value.nil? ? "(" : "(#{color_option_value.value} " 
-        upholstery_option_value = o.find { |option| option.option_name == "Upholstery"}
-        pseudo_product_title += upholstery_option_value.nil? ? ")" : "#{upholstery_option_value.value})"
-        length_option_value = o.find { |option| option.option_name == "Lengths" }
-        length_option_value = length_option_value.value unless length_option_value.nil?
-        depth_option_value = o.find { |option| option.option_name == "Depth" }
-        depth_option_value = depth_option_value.value unless depth_option_value.nil?
-        pseudo_product_title = "#{length_option_value} #{depth_option_value} #{pseudo_product_title}"
-        pseudo_product = ShopifyAPI::Product.create(title: "#{pseudo_product_title}") 
-        pseudo_product_variant = pseudo_product.variants.first
-        pseudo_product_variant.update_attributes(:option1 => pseudo_product_title, :price => variant.price, :sku => variant.sku)
-        variant.update_attributes(:pseudo_product_id => pseudo_product.id, :pseudo_product_variant_id => pseudo_product_variant.id)
-      end
-
+      variant = Variant.find_by( :product_id => _product.id, :sku => variants[i]['sku'] ) unless variants[i]['sku'].blank?
+      variant = Variant.create( :product_id => _product.id, :sku => variants[i]['sku'], :price => variants[i]['price'], :length => variants[i]['length'], :height => variants[i]['height'], :depth => variants[i]['depth'], :state => variants[i]['state']) if variant.blank?
+                                
       variants[i]['options'].keys.each do |key|
         option = Option.where(name: key).first || Option.create(name: key)
         if option.products_options.where(:product_id => _product.id).empty?
@@ -160,8 +139,8 @@ module ProductsHelper
         end
         unless variants[i]['options'][key].nil? 
           curr_option_value = variant.option_values.where( option_id: option.id ).first
-          new_option_value = OptionValue.where( :value => variants[i]['options'][key].capitalize ).first
-          new_option_value = OptionValue.create :option_id => option.id, :value => variants[i]['options'][key].capitalize if new_option_value.nil?
+          new_option_value = OptionValue.where( :option_id => option.id, :value => variants[i]['options'][key].titleize ).first
+          new_option_value = OptionValue.create( :option_id => option.id, :value => variants[i]['options'][key].titleize ) if new_option_value.nil?
           
           if curr_option_value.present? 
             if curr_option_value.value != variants[i]['options'][key]
@@ -172,14 +151,24 @@ module ProductsHelper
             variant.option_values << new_option_value
           end
         end
-      end
+      end                                
+                                
+      pseudo_product_title = "#{_product.title} "
+      o = variant.option_values.joins("join options on options.id = option_values.option_id").select("options.name as option_name, option_values.value as value")
+      color_option_value = o.find { |option| option.option_name == "Color" }
+      pseudo_product_title += color_option_value.nil? ? "(" : "(#{color_option_value.value} " 
+      upholstery_option_value = o.find { |option| option.option_name == "Upholstery"}
+      pseudo_product_title += upholstery_option_value.nil? ? ")" : "#{upholstery_option_value.value})"
+      length_option_value = o.find { |option| option.option_name == "Lengths" }
+      length_option_value = length_option_value.value unless length_option_value.nil?
+      depth_option_value = o.find { |option| option.option_name == "Depth" }
+      depth_option_value = depth_option_value.value unless depth_option_value.nil?
+      pseudo_product_title = "#{length_option_value} #{depth_option_value} #{pseudo_product_title}"
       
-      variant_keys = variant.attributes.keys.collect! { |x| x unless FORBIDDEN_FIELDS.include? x }.compact!
-      variant_hash = {}
-      variant_keys.each do |key|
-        variant_hash[key.to_sym] = variants[i][key]
-      end
-      variant.update_attributes variant_hash
+      pseudo_product = ShopifyAPI::Product.create(title: "#{pseudo_product_title}") 
+      pseudo_product_variant = pseudo_product.variants.first
+      pseudo_product_variant.update_attributes(:option1 => pseudo_product_title, :price => variant.price, :sku => variant.sku)
+      variant.update_attributes(:pseudo_product_id => pseudo_product.id, :pseudo_product_variant_id => pseudo_product_variant.id)
       
       unless variants[i]['three_sixty_image'].blank?
         imgSet = ThreeSixtyImage.where( :title => variants[i]['three_sixty_image'] ).first
@@ -206,7 +195,6 @@ module ProductsHelper
         variant.three_sixty_image = imgSet
       end
       variant.save
-      
     end     
   end
   
@@ -257,11 +245,12 @@ module ProductsHelper
     variants
   end
   
-  
   def get_product_data product
     product_keys = product.attributes.keys 
     product_keys.collect! { |x| x unless FORBIDDEN_FIELDS.include? x }.compact!
-    Hash[product_keys.collect { |v| [v, eval("product.#{v}")] }]
+    data = Hash[product_keys.collect { |v| [v, eval("product.#{v}")] }]
+    data['images'] = product.images.map{ |img| img.src }.join(',')
+    data
   end
   
   def get_product_info product
@@ -277,14 +266,11 @@ module ProductsHelper
     _variants_a = product_variants.collect do |p|
       variant_keys = p.attributes.keys.collect! { |x| x unless FORBIDDEN_FIELDS.include? x }.compact!
       _h = Hash[variant_keys.collect { |v| [v, eval("p.#{v}")] } ]
-      
-      # _h['shipping_method_name'] = p.shipping_method.name
-      
       _h['options'] = options.collect do |option|
         value = p.option_values.find_by(option_id: option.id) 
         Hash[ option.name, value.nil? ? nil : value.value ]
       end
-      
+     
       unless p.three_sixty_image.nil?
         _h['three_sixty_image'] = ThreeSixtyImage.find(p.three_sixty_image_id).title
         tsi = ThreeSixtyImage.find(p.three_sixty_image_id)
